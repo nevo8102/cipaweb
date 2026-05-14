dd0cb6a9a7754349b83258bfabcb188e20b5dc1c
nevo8102 <rosenevo@gmail.com>
2026-05-14 15:21:00 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 'kipa-001',
+    name: 'כיפת קטיפה שחורה',
+    image: 'https://images.unsplash.com/photo-1461360370896-922624d12aa1?auto=format&fit=crop&w=900&q=80',
+    color: 'שחור',
+    size: '5',
+    price: 79,
+    description: 'כיפת קטיפה קלאסית בגימור יוקרתי לאירועים ולשבת.'
+  },
+  {
+    id: 'kipa-002',
+    name: 'כיפה סרוגה כחול נייבי',
+    image: 'https://images.unsplash.com/photo-1519817650390-64a93db51149?auto=format&fit=crop&w=900&q=80',
+    color: 'כחול נייבי',
+    size: '6',
+    price: 59,
+    description: 'כיפה סרוגה נוחה וקלה ליום יום בעיצוב אלגנטי.'
+  },
+  {
+    id: 'kipa-003',
+    name: 'כיפת זמש חום כהה',
+    image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&w=900&q=80',
+    color: 'חום',
+    size: '5.5',
+    price: 89,
+    description: 'מראה טבעי ועדין, מתאימה ללבוש מכובד וחגיגי.'
+  },
+  {
+    id: 'kipa-004',
+    name: 'כיפה לבנה קלאסית',
+    image: 'https://images.unsplash.com/photo-1520975922284-9d6f81f9f8bd?auto=format&fit=crop&w=900&q=80',
+    color: 'לבן',
+    size: '4',
+    price: 49,
+    description: 'כיפה לבנה קלאסית, אוורירית ונעימה במיוחד.'
+  },
+  {
+    id: 'kipa-005',
+    name: 'כיפת קטיפה כחולה',
+    image: 'https://images.unsplash.com/photo-1441986300917-64674bd600d8?auto=format&fit=crop&w=900&q=80',
+    color: 'כחול',
+    size: '6',
+    price: 82,
+    description: 'קטיפה רכה בגוון כחול עמוק עם מראה יוקרתי.'
+  },
+  {
+    id: 'kipa-006',
+    name: 'כיפה סרוגה אפורה',
+    image: 'https://images.unsplash.com/photo-1521572267360-ee0c2909d518?auto=format&fit=crop&w=900&q=80',
+    color: 'אפור',
+    size: '5',
+    price: 54,
+    description: 'כיפה מודרנית שמתאימה לכל לבוש ולכל גיל.'
+  },
+  {
+    id: 'kipa-007',
+    name: 'כיפת פשתן בז׳',
+    image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?auto=format&fit=crop&w=900&q=80',
+    color: 'בז׳',
+    size: '5.5',
+    price: 73,
+    description: 'בד פשתן איכותי למראה נקי ואלגנטי.'
+  },
+  {
+    id: 'kipa-008',
+    name: 'כיפה מעוטרת זהב עדין',
+    image: 'https://images.unsplash.com/photo-1523381210434-271e8be1f52b?auto=format&fit=crop&w=900&q=80',
+    color: 'זהב',
+    size: '6.5',
+    price: 99,
+    description: 'כיפה חגיגית עם עיטור עדין, מושלמת לאירועים מיוחדים.'
+  }
+];
